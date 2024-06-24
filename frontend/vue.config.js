203abcd1cad0d655882bd4cd7ec575ddeecd4f44
Vimos <vimostan@gmail.com>
2024-06-24 11:13:25 +0800

    use LF not CRLF on git

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,6 +8,16 @@ module.exports = defineConfig({
                 args[0].title = 'CAS-SIAT-Fintech: AutoInvoice'
                 return args
             })
+        const svgRule = config.module.rule('svg')
+
+        svgRule.uses.clear()
+
+        svgRule
+            .use('babel-loader')
+            .loader('babel-loader')
+            .end()
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader')
     },
     outputDir: 'dist',
     assetsDir: 'static',
