aed9b2fdc6948b3671d54f697c9f450a0815e182
Chihiro <66126634+Sept6VirD@users.noreply.github.com>
2024-07-13 14:10:42 +0800

    增加了自动摘要功能

--- backend/src/xinhai/workers/storage.py
+++ backend/src/xinhai/workers/storage.py
@@ -19,7 +19,7 @@ import uvicorn
 from PIL import Image
 from chromadb.utils import embedding_functions
 from fastapi import FastAPI, Request
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from ..config import LOG_DIR, WORKER_HEART_BEAT_INTERVAL, STATIC_PATH
 from ..utils import build_logger, pretty_print_semaphore
@@ -41,6 +41,7 @@ NO_REGISTER = os.environ.get("NO_REGISTER", False)
 MODEL_NAME = os.environ.get("MODEL_NAME", "storage")
 LIMIT_MODEL_CONCURRENCY = int(os.environ.get("LIMIT_MODEL_CONCURRENCY", 5))
 EMBEDDING_MODEL_PATH = os.environ.get("EMBEDDING_MODEL_PATH")
+SUMMARY_MODEL_PATH = os.environ.get("SUMMARY_MODEL_PATH")
 DEVICE = "cuda"
 
 embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_MODEL_PATH)
@@ -56,6 +57,12 @@ class StorageWorker:
     def __init__(self):
         self.client = chromadb.PersistentClient(path=DB_PATH)
         self.embedding_fn = embedding_fn
+        self.summary_model = AutoModelForCausalLM.from_pretrained(
+            SUMMARY_MODEL_PATH,
+            torch_dtype="auto",
+            device_map=DEVICE
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(SUMMARY_MODEL_PATH)
 
         if not NO_REGISTER:
             self.register_to_controller()
@@ -109,6 +116,41 @@ class StorageWorker:
             "speed": 1,
             "queue_length": self.get_queue_length(),
         }
+        
+    def get_summary(self, pre_dialogue_summary, dialogue):
+        prompt = f"""请根据以前的对话摘要和新的对话内容，给出新的对话摘要总结。
+                新的对话摘要应当包含之前摘要的内容。
+                摘要长度不应过长或过短，应该根据之前对话摘要和对话内容而定。
+                 ####
+                 以前的对话摘要：{pre_dialogue_summary}
+                 
+                 ####
+                 新的对话内容：{dialogue}
+                 
+                 ###Attention###
+                 仅返回新的对话摘要内容，不要返回分析过程！
+                """
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(DEVICE)
+
+        generated_ids = self.summary_model.generate(
+            model_inputs.input_ids,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return response
 
     def insert_data(self, params):
         ### metadatas的格式：[{"source": "Human"}, {"source": "AI"}]
@@ -126,6 +168,20 @@ class StorageWorker:
             name="User_" + str(user_id),
             embedding_function=self.embedding_fn
         )
+        collection_summary = self.client.get_or_create_collection(
+            name="User_" + str(user_id) + "summary",
+            embedding_function=self.embedding_fn
+        )
+        dialogues = [f'{meta["source"]}:{doc}' for meta, doc in zip(metadatas, documents)]
+        for dialogue in dialogues:
+            pre_dialogue_summaries = collection_summary.get(include=['documents'])['documents']
+            index = collection_summary.count()
+            if index == 0:
+                new_summary = ""
+            else:
+                new_summary = self.get_summary(pre_dialogue_summaries[index-1], dialogue)
+            collection_summary.add(documents=[new_summary], ids=[str(collection_summary.count()+1)])
+        logger.info(f'User_{user_id}\'s summary_storage adds a message')
         res = collection.count()
         ids = [f'{user_id}_{i + res}' for i in range(len(documents))]
         collection.add(documents=documents, ids=ids, metadatas=metadatas)
@@ -133,11 +189,12 @@ class StorageWorker:
         return json.dumps({
             "user_id": user_id,
             "new_document_count": len(documents),
+            "new_summary_count":len(dialogues),
             "error_code": 0
         })
 
     def get_data(self, params):
-        ### 返回第k次及之后的对话
+        ### 返回第k次及之后的对话,以及第k-1次的摘要
         user_id = params.get('user_id')
         k = params.get('k', 0)
         
@@ -149,12 +206,16 @@ class StorageWorker:
                 
         collection = self.client.get_or_create_collection(name="User_" + str(user_id),
                                                                embedding_function=self.embedding_fn)
+        collection_summary = self.client.get_or_create_collection(name="User_" + str(user_id) + "summary",
+                                                               embedding_function=self.embedding_fn)
         dialogues = collection.get(include=['documents'])['documents']
+        summary_dialogues = collection_summary.get(include=['documents'])['documents']
         sources = collection.get(include=['metadatas'])['metadatas']
         res = collection.count()
         results = []
+        results.append("Previous dialogues' summaries: " + summary_dialogues[k-1])
         for i in range(k, res):
-            results.append(sources[i]['source:  '] + dialogues[i])
+            results.append(sources[i]['source'] + ": "+ dialogues[i])
         return json.dumps({
             "user_id": user_id,
             "dialogues": results,
