1118f9a22e8aa8b394ff253d916a09c33fd5ff3b
Diana <53417050+BestLemoon@users.noreply.github.com>
2024-07-22 15:50:56 +0800

    add cpsyexam.py and requirements.txt

--- preprocessing/cpsyexam.py
+++ preprocessing/cpsyexam.py
@@ -3,9 +3,9 @@ import sys
 import json
 from zipfile import ZipFile
 from langchain.vectorstores import Chroma
-'''
-python script.py cpsyexam.zip chroma_path
-'''
+from langchain.embeddings import SentenceTransformerEmbeddings
+
+
 def unzip_files(zip_path, extract_to):
     with ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
@@ -21,7 +21,17 @@ def get_files(zip_path):
                 yield os.path.join(folder_path, filename), f"{'CA' if filename.startswith('CA-') else 'KG'}_{split}"
 
 
-def process_files(zip_path, chroma_path):
+def concatenate_fields(doc):
+    # Combine subject_name, question, options, and explanation into one string
+    options_str = " ".join(f"{key}: {value}" for key, value in doc["options"].items() if value.strip())
+    combined_str = f"{doc['subject_name']} {doc['Question']} {options_str} {doc['explanation']}"
+    return combined_str
+
+
+def process_files(zip_path, chroma_path, model_path):
+    # Initialize embeddings with the specified model
+    embeddings = SentenceTransformerEmbeddings(model_name=model_path)
+
     for file_path, collection_name in get_files(zip_path):
         print(f"Processing file: {file_path} for collection: {collection_name}")
 
@@ -29,24 +39,27 @@ def process_files(zip_path, chroma_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             documents = json.load(f)
 
+        # Extract, concatenate fields, and embed explanations
+        explanations = [(doc['id'], embeddings.embed(concatenate_fields(doc))) for doc in documents]
+
         # Define the path for Chroma database
         db_path = os.path.join(chroma_path, collection_name)
 
         # Check if the Chroma DB for this collection already exists
         if not os.path.exists(db_path):
             os.makedirs(db_path, exist_ok=True)
-            db = Chroma.from_documents(documents, persist_directory=db_path)
+            db = Chroma.from_documents([(doc[0], doc[1]) for doc in explanations], persist_directory=db_path)
         else:
             # Load the existing Chroma DB
             db = Chroma(persist_directory=db_path)
-        db.add_documents(documents)
+            db.add_documents([(doc[0], doc[1]) for doc in explanations])
 
         print(f"Finished processing for collection: {collection_name}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <ZIP_PATH> <CHROMA_PATH>")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <ZIP_PATH> <MODEL_PATH> <CHROMA_PATH> ")
     else:
-        zip_path, chroma_path = sys.argv[1:]
-        process_files(zip_path, chroma_path)
+        zip_path, model_path, chroma_path = sys.argv[1:]
+        process_files(zip_path, chroma_path, model_path)
