fced524378ab17592ccb14de4b750a7869244031
Vimos <vimostan@gmail.com>
2024-07-25 23:10:36 +0800

    Add support for multiple language

--- backend/src/xinhai/types/routing.py
+++ backend/src/xinhai/types/routing.py
@@ -8,20 +8,39 @@ Authors: Vimos Tan
 """
 from dataclasses import dataclass
 from enum import Enum
+from typing import Dict
+
+from xinhai.types.i18n import XinHaiI18NLocales
 
 
 @dataclass
 class XinHaiRoutingTypeMixin:
     routing_name: str
-    description: str
+    description: Dict[XinHaiI18NLocales, str]
 
 
 class XinHaiRoutingType(XinHaiRoutingTypeMixin, Enum):
-    SINGLE_CAST = "[SingleCast]", "单播，发给一个可通讯的智能体"
-    MULTICAST = "[Multicast]", "组播，发给若干可通讯的智能体"
-    BROADCAST = "[Broadcast]", "广播，发给所有可通讯的智能体"
-    END_CAST = "[EndCast]", "终止，终止与指定智能体之间的通讯"
+    LOOPBACK = "[LOOPBACK]", {
+        XinHaiI18NLocales.CHINESE: "环回，继续进行陈述"
+    }
+
+    SINGLE_CAST = "[SingleCast]", {
+        XinHaiI18NLocales.CHINESE: "单播，发给一个可通讯的智能体"
+    }
+
+    MULTICAST = "[Multicast]", {
+        XinHaiI18NLocales.CHINESE: "组播，发给若干可通讯的智能体"
+    }
+
+    BROADCAST = "[Broadcast]", {
+        XinHaiI18NLocales.CHINESE: "广播，发给所有可通讯的智能体"
+    }
+
+    END_CAST = "[EndCast]", {
+        XinHaiI18NLocales.CHINESE: "终止，终止与指定智能体之间的通讯"
+    }
 
     @classmethod
-    def to_description(cls):
-        return "\n".join(f"{member.routing_name}: {member.description}" for name, member in cls.__members__.items())
+    def to_description(cls, locale: XinHaiI18NLocales) -> str:
+        return "\n".join(
+            f"{member.routing_name}: {member.description[locale]}" for name, member in cls.__members__.items())
