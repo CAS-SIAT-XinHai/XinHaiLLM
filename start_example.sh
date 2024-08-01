39cbe8a244f264d51fdba7e17f7f5d5e013a89a5
Vimos <vimostan@gmail.com>
2024-08-01 16:25:06 +0800

    Update start example

--- start_example.sh
+++ start_example.sh
@@ -6,7 +6,7 @@ echo "${UUID}"
 PID=$BASHPID
 echo "$PID"
 CONDA_HOME=/home/tanminghuan/anaconda3
-CONDA_ENV=AutoInvoice
+CONDA_ENV=base
 
 OUTPUT_DIR="${WORK_DIR}"/output
 mkdir -p "${OUTPUT_DIR}"
@@ -39,24 +39,49 @@ echo "$start_webui_script"
 #screen -dmS start_webui_$PID bash -c "$start_webui_script"
 tmux new-session -d -s xinhai_webui_$PID "$start_webui_script"
 
-start_controller_script="cd ${WORK_DIR}/backend/src && python -m xinhai.controller --host 0.0.0.0 --port 5000"
+CONTROLLER_HOST=0.0.0.0
+CONTROLLER_PORT=5000
+CONTROLLER_ADDRESS=http://localhost:5000
+
+OCR_WORKER_PORT=40000
+LLM_WORKER_PORT=40001
+MLLM_WORKER_PORT=40002
+KNOWLEDGE_WORKER_PORT=40003
+STORAGE_WORKER_PORT=40004
+AGENCY_WORKER_PORT=40005
+
+OCR_DEVICE=0
+LLM_DEVICE=0
+MLLM_DEVICE=1
+KNOWLEDGE_DEVICE=0
+STORAGE_DEVICE=0
+
+LD_LIBRARY_PATH=/home/tanminghuan/anaconda3/lib/
+LLM_MODEL_PATH=/data/pretrained_models/Qwen1.5-7B-Chat
+LLM_MODEL_NAME=Qwen1.5-7B-Chat
+LLM_MODEL_TEMPLATE=qwen
+MLLM_MODEL_PATH=/data/pretrained_models/MiniCPM-V-2
+MLLM_MODEL_NAME=MiniCPM-V-2
+MLLM_MODEL_TEMPLATE=cpm
+
+start_controller_script="cd ${WORK_DIR}/backend/src && python -m xinhai.controller --host ${CONTROLLER_HOST} --port ${CONTROLLER_PORT}"
 echo "$start_controller_script"
 #screen -dmS start_webui_$PID bash -c "$start_webui_script"
 tmux new-session -d -s xinhai_controller_$PID "$start_controller_script"
 
 sleep 10
 
-start_ocr_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 LD_LIBRARY_PATH=/home/tanminghuan/anaconda3/lib/ CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=paddleocr WORKER_ADDRESS=http://localhost:40000 WORKER_HOST=0.0.0.0 WORKER_PORT=40000 python -m xinhai.workers.ocr"
+start_ocr_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=${OCR_DEVICE} LD_LIBRARY_PATH=${LD_LIBRARY_PATH} CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=paddleocr WORKER_ADDRESS=http://localhost:${OCR_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${OCR_WORKER_PORT} python -m xinhai.workers.ocr"
 echo "$start_ocr_script"
 #screen -dmS start_ocr_$PID bash -c "$start_ocr_script"
 tmux new-session -d -s xinhai_ocr_$PID "$start_ocr_script"
 
-start_llm_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 PYTHONPATH=${WORK_DIR}/related_repos/LLaMA-Factory/src CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=Qwen1.5-7B-Chat WORKER_ADDRESS=http://localhost:40001 WORKER_HOST=0.0.0.0 WORKER_PORT=40001 python -m xinhai.workers.llm --model_name_or_path /data2/public/pretrained_models/Qwen1.5-7B-Chat --template qwen --infer_backend vllm --vllm_enforce_eager --infer_dtype float16"
+start_llm_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=${LLM_DEVICE} PYTHONPATH=${WORK_DIR}/related_repos/LLaMA-Factory/src CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=${LLM_MODEL_NAME} WORKER_ADDRESS=http://localhost:${LLM_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${LLM_WORKER_PORT} python -m xinhai.workers.llm --model_name_or_path ${LLM_MODEL_PATH} --template ${LLM_MODEL_TEMPLATE} --infer_backend vllm --vllm_enforce_eager --infer_dtype float16"
 echo "$start_llm_script"
 #screen -dmS start_llm_$PID bash -c "$start_llm_script"
 tmux new-session -d -s xinhai_llm_$PID "$start_llm_script"
 
-start_mllm_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=MiniCPMV WORKER_ADDRESS=http://localhost:40004 WORKER_HOST=0.0.0.0 WORKER_PORT=40004 python -m xinhai.workers.mllm --model_name_or_path /data/xuancheng/MiniCPM-V-2 --template cpm --infer_backend vllm --vllm_enforce_eager --infer_dtype float16"
+start_mllm_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=${MLLM_DEVICE} PYTHONPATH=${WORK_DIR}/related_repos/LLaMA-Factory/src CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=${MLLM_MODEL_NAME} WORKER_ADDRESS=http://localhost:${MLLM_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${MLLM_WORKER_PORT} python -m xinhai.workers.mllm --model_name_or_path ${MLLM_MODEL_PATH} --template ${MLLM_MODEL_TEMPLATE} --infer_backend vllm --vllm_enforce_eager --infer_dtype float16"
 echo "$start_mllm_script"
 #screen -dmS start_mllm_$PID bash -c "$start_mllm_script"
 tmux new-session -d -s xinhai_mllm_$PID "$start_mllm_script"
@@ -65,7 +90,7 @@ PRO_KNOWLEDGE_DB_PATH=/data/pretrained_models/ProDB-bge-1.5-300
 SS_KNOWLEDGE_DB_PATH=/data/pretrained_models/KnowDB-bge-1.5-300
 EMBEDDING_MODEL_PATH=/data/pretrained_models/bge-large-zh-v1.5
 RERANKER_MODEL_PATH=/data/pretrained_models/maidalun/bce-reranker-base_v1
-start_knowledge_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=knowledge WORKER_ADDRESS=http://localhost:40002 WORKER_HOST=0.0.0.0 WORKER_PORT=40002 PRO_KNOWLEDGE_DB_PATH=${PRO_KNOWLEDGE_DB_PATH} SS_KNOWLEDGE_DB_PATH=${SS_KNOWLEDGE_DB_PATH} EMBEDDING_MODEL_PATH=${EMBEDDING_MODEL_PATH} RERANKER_MODEL_PATH=${RERANKER_MODEL_PATH} python -m xinhai.workers.knowledge"
+start_knowledge_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=${KNOWLEDGE_DEVICE} CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=knowledge WORKER_ADDRESS=http://localhost:${KNOWLEDGE_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${KNOWLEDGE_WORKER_PORT} PRO_KNOWLEDGE_DB_PATH=${PRO_KNOWLEDGE_DB_PATH} SS_KNOWLEDGE_DB_PATH=${SS_KNOWLEDGE_DB_PATH} EMBEDDING_MODEL_PATH=${EMBEDDING_MODEL_PATH} RERANKER_MODEL_PATH=${RERANKER_MODEL_PATH} python -m xinhai.workers.knowledge"
 echo "$start_knowledge_script"
 #screen -dmS start_ocr_$PID bash -c "$start_ocr_script"
 tmux new-session -d -s xinhai_knowledge_$PID "$start_knowledge_script"
@@ -73,11 +98,12 @@ tmux new-session -d -s xinhai_knowledge_$PID "$start_knowledge_script"
 DB_PATH=/data/pretrained_models/KnowDB-bge-1.5-300
 EMBEDDING_MODEL_PATH=/data/pretrained_models/bge-large-zh-v1.5
 RERANKER_MODEL_PATH=/data/pretrained_models/maidalun/bce-reranker-base_v1
-start_storage_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=storage WORKER_ADDRESS=http://localhost:40003 WORKER_HOST=0.0.0.0 WORKER_PORT=40003 DB_PATH=${DB_PATH} EMBEDDING_MODEL_PATH=${EMBEDDING_MODEL_PATH} python -m xinhai.workers.storage"
+start_storage_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=${STORAGE_DEVICE} CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=storage WORKER_ADDRESS=http://localhost:${STORAGE_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${STORAGE_WORKER_PORT} DB_PATH=${DB_PATH} EMBEDDING_MODEL_PATH=${EMBEDDING_MODEL_PATH} python -m xinhai.workers.storage"
 echo "$start_storage_script"
 tmux new-session -d -s xinhai_storage_$PID "$start_storage_script"
 
 AGENCY_CONFIG_PATH=${WORK_DIR}/configs/xinhai_cbt_agency.yaml
-start_agency_script="cd ${WORK_DIR}/backend/src && CUDA_VISIBLE_DEVICES=0 PYTHONPATH=${WORK_DIR}/related_repos/LLaMA-Factory/src CONTROLLER_ADDRESS=http://localhost:5000 MODEL_NAME=${AGENCY_CONFIG_PATH} WORKER_ADDRESS=http://localhost:40004 WORKER_HOST=0.0.0.0 WORKER_PORT=40004 AGENCY_CONFIG_PATH=${AGENCY_CONFIG_PATH} python -m xinhai.workers.agency"
+AGENCY_NAME=xinhai_cbt_agency
+start_agency_script="cd ${WORK_DIR}/backend/src && PYTHONPATH=${WORK_DIR}/related_repos/LLaMA-Factory/src CONTROLLER_ADDRESS=${CONTROLLER_ADDRESS} MODEL_NAME=${AGENCY_NAME} WORKER_ADDRESS=http://localhost:${AGENCY_WORKER_PORT} WORKER_HOST=0.0.0.0 WORKER_PORT=${AGENCY_WORKER_PORT} AGENCY_CONFIG_PATH=${AGENCY_CONFIG_PATH} python -m xinhai.workers.agency"
 echo "$start_agency_script"
 tmux new-session -d -s xinhai_agency_$PID "$start_agency_script"
