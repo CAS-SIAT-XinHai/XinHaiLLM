23cfcb37a15167a669516299ac21b992fb2deb4e
koenshen <1101881833@qq.com>
2024-08-24 21:22:41 +0800

    reformat code

--- examples/AutoCBT/autocbt_single_qa.py
+++ examples/AutoCBT/autocbt_single_qa.py
@@ -6,7 +6,8 @@ import yaml
 import chromadb
 import datetime
 
-def sort_message_of_history(data_list:list):
+
+def sort_message_of_history(data_list: list):
     parsed_list = []
     # 假设 data_list 是你的原始列表
     for item in data_list:
@@ -21,12 +22,14 @@ def sort_message_of_history(data_list:list):
     sorted_list = sorted(parsed_list, key=lambda x: x["indexId"], reverse=False)
     return sorted_list
 
+
 if __name__ == "__main__":
     # start1()
     for language in ["en", "zh"]:
         dataset_name = "psyqa_balanced" if language == "zh" else "therapistqa_balanced"
         environment_id = str(uuid.uuid4())
-        with open(f'/data/xuancheng/koenshen/XinHaiLLM_240821/examples/AutoCBT/configs/xinhai_cbt_{language}.yaml', 'r', encoding='utf-8') as f:
+        with open(f'/data/xuancheng/koenshen/XinHaiLLM_240821/examples/AutoCBT/configs/xinhai_cbt_{language}.yaml', 'r',
+                  encoding='utf-8') as f:
             config = yaml.safe_load(f)
         with open(f"/data/xuancheng/{dataset_name}.json", 'r', encoding='utf-8') as f:
             ques_descs = json.load(f)
@@ -40,13 +43,16 @@ if __name__ == "__main__":
             res = []
             config['arena']['environment']['environment_id'] = environment_id
             config['arena']['agents'][0]['role_description'] = texts[i]
-            with open(f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml', 'w', encoding='utf-8') as f:
+            with open(f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml', 'w',
+                      encoding='utf-8') as f:
                 yaml.safe_dump(config, f, allow_unicode=True)
 
             always_repeate = True
             while always_repeate:
                 try:
-                    subprocess.run(['python', '-m', 'xinhai.arena.simulation', '--config_path', f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml'], cwd=work_dir)
+                    subprocess.run(['python', '-m', 'xinhai.arena.simulation', '--config_path',
+                                    f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml'],
+                                   cwd=work_dir)
                 except Exception as e:
                     print(f"An error occurred: {e}")
                     pass
@@ -61,7 +67,7 @@ if __name__ == "__main__":
                     aa = metadata['message']
                     if "咨询者" in aa['username'] or "user" in aa['username']:
                         if second_meet_user:
-                            final_answer = metadatas[index-1]['message']["content"]
+                            final_answer = metadatas[index - 1]['message']["content"]
                             always_repeate = False
                             ques_descs[i]["cbt_answer"] = final_answer
                             ques_descs[i]["cbt_history"] = history
@@ -74,5 +80,6 @@ if __name__ == "__main__":
                     history.append(dialogue)
                 # 如果always_repeate依旧是True，说明咨询师在10轮对话中还没回复咨询者，开始新一轮的对话
 
-        with open(f'/data/xuancheng/final_cbtagency/single_turn/{dataset_name}_cbtagency_{language}.json', 'w', encoding='utf-8')as f:
+        with open(f'/data/xuancheng/final_cbtagency/single_turn/{dataset_name}_cbtagency_{language}.json', 'w',
+                  encoding='utf-8') as f:
             json.dump(result, f, indent=4, ensure_ascii=False)
