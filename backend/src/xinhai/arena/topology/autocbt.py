20e4f2c1df082c6049ac71346f415bd2e966e72e
koenshen <1101881833@qq.com>
2024-08-29 07:31:02 +0800

    去除注释

--- backend/src/xinhai/arena/topology/autocbt.py
+++ backend/src/xinhai/arena/topology/autocbt.py
@@ -1,11 +1,3 @@
-"""
-Copyright (c) CAS-SIAT-XinHai.
-Licensed under the CC0-1.0 license.
-
-XinHai stands for [Sea of Minds].
-
-Authors: Vimos Tan
-"""
 from typing import Any, List
 
 import networkx as nx
