2e183ca66fdc23876c560051a9dc7094ed437922
koenshen <1101881833@qq.com>
2024-08-29 07:28:19 +0800

    多线程跑单轮qa

--- examples/AutoCBT/autocbt_single_qa.py
+++ examples/AutoCBT/autocbt_single_qa.py
@@ -5,7 +5,60 @@ import subprocess
 import yaml
 import chromadb
 import datetime
+import threading
+import pytz
 
+# project_path = "/data/xuancheng/koenshen/XinHaiLLM_240821"
+# data_path = "/data/xuancheng/koenshen/XinHaiLLM_240821/data"
+# db_path = "/data/xuancheng/koenshen/AutoCBT-DB"
+
+project_path = "/data/yangmin/autocbt/XinHaiLLM"
+data_path = "/data/yangmin/autocbt/data"
+db_path = "/data/yangmin/autocbt/AutoCBT-DB"
+
+client = chromadb.PersistentClient(path=db_path)
+
+def hello(config:dict, role_description:str, data_dict: dict) -> dict:
+    res = []
+    environment_id = str(uuid.uuid4())
+    config['arena']['environment']['environment_id'] = environment_id
+    config['arena']['agents'][0]['role_description'] = role_description
+    with open(f'{data_path}/configs/{environment_id}.yaml', 'w', encoding='utf-8') as f:
+        yaml.safe_dump(config, f, allow_unicode=True)
+
+    try:
+        subprocess.run(['python', '-m', 'xinhai.arena.simulation', '--config_path', f'{data_path}/configs/{environment_id}.yaml'], cwd=work_dir)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        pass
+
+    target_whole = client.get_collection(f"{environment_id}-1")
+    unsorted_metadatas_whole = target_whole.get(include=['metadatas'])['metadatas']
+    metadatas_whole = sort_message_of_history(unsorted_metadatas_whole)
+
+    final_answer = metadatas_whole[-1]['message']['content']
+    data_dict["cbt_answer"] = final_answer
+    data_dict["cbt_history"] = metadatas_whole
+    beijing_tz = pytz.timezone('Asia/Shanghai')  # 创建北京时间（亚洲/上海）时区对象
+    beijing_time = datetime.datetime.now(beijing_tz).strftime('%Y-%m-%d %H:%M:%S')  # 获取当前的北京时间
+    data_dict["cbt_generate_time"] = beijing_time
+    data_dict["environment_id"] = environment_id
+
+    return data_dict
+
+def process_data(data_list: list, start_index: int, end_index: int):
+    temp_list = data_list[start_index: end_index]
+    result_list = []
+    for index, data_dict in enumerate(temp_list):
+        print(f"XXX {index} : {start_index}->{end_index} XXXXXXXXXXXXXXXXXXXXXXXXX\n")
+        role_description = data_dict['question'] + data_dict['description']
+        with open(f'{project_path}/examples/AutoCBT/configs/xinhai_cbt_{language}.yaml', 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        result_dict = hello(config, role_description, data_dict)
+        result_list.append(result_dict)
+
+        with open(f"{data_path}/result/therapistqa_balanced_cbtagency_{start_index}_{end_index}.json", 'w', encoding='utf-8') as f:
+            json.dump(result_list, f, indent=4, ensure_ascii=False)
 
 def sort_message_of_history(data_list: list):
     parsed_list = []
@@ -22,64 +75,25 @@ def sort_message_of_history(data_list: list):
     sorted_list = sorted(parsed_list, key=lambda x: x["indexId"], reverse=False)
     return sorted_list
 
-
 if __name__ == "__main__":
-    # start1()
-    for language in ["en", "zh"]:
+    for language in ["en"]:
         dataset_name = "psyqa_balanced" if language == "zh" else "therapistqa_balanced"
-        environment_id = str(uuid.uuid4())
-        with open(f'/data/xuancheng/koenshen/XinHaiLLM_240821/examples/AutoCBT/configs/xinhai_cbt_{language}.yaml', 'r',
-                  encoding='utf-8') as f:
-            config = yaml.safe_load(f)
-        with open(f"/data/xuancheng/{dataset_name}.json", 'r', encoding='utf-8') as f:
-            ques_descs = json.load(f)
-        client = chromadb.PersistentClient(path='/data/pretrained_models/AutoCBT-DB')
-        texts = []
-        for ques_desc in ques_descs:
-            texts.append(ques_desc['question'] + ques_desc['description'])
-        work_dir = '/data/xuancheng/koenshen/XinHaiLLM_240821/backend/src'
-        result = []
-        for i in range(0, 100):
-            res = []
-            config['arena']['environment']['environment_id'] = environment_id
-            config['arena']['agents'][0]['role_description'] = texts[i]
-            with open(f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml', 'w',
-                      encoding='utf-8') as f:
-                yaml.safe_dump(config, f, allow_unicode=True)
 
-            always_repeate = True
-            while always_repeate:
-                try:
-                    subprocess.run(['python', '-m', 'xinhai.arena.simulation', '--config_path',
-                                    f'/data/xuancheng/final_cbtagency/configs/xinhai_cbt_{language}_single_turn_{i}.yaml'],
-                                   cwd=work_dir)
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-                    pass
-                target = client.get_collection(f"{environment_id}-0")
-                unsorted_metadatas = target.get(include=['metadatas'])['metadatas']
-                metadatas = sort_message_of_history(unsorted_metadatas)
+        with open(f"{data_path}/{dataset_name}.json", 'r', encoding='utf-8') as f:
+            data_list = json.load(f)
+        work_dir = f'{project_path}/backend/src'
 
-                second_meet_user = False
-                history = []
-                final_answer = ""
-                for index, metadata in enumerate(metadatas):
-                    aa = metadata['message']
-                    if "咨询者" in aa['username'] or "user" in aa['username']:
-                        if second_meet_user:
-                            final_answer = metadatas[index - 1]['message']["content"]
-                            always_repeate = False
-                            ques_descs[i]["cbt_answer"] = final_answer
-                            ques_descs[i]["cbt_history"] = history
-                            ques_descs[i]["cbt_generate_time"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                            result.append(ques_descs[i])
-                            break
-                        else:
-                            second_meet_user = True
-                    dialogue = aa['username'] + "：" + aa['content']
-                    history.append(dialogue)
-                # 如果always_repeate依旧是True，说明咨询师在10轮对话中还没回复咨询者，开始新一轮的对话
+        num_threads = 10
+        num_dicts_per_thread = len(data_list) // num_threads
+        threads = []
+        for i in range(num_threads):
+            print(f"=================== {i} start==========================")
+            start_index = i * num_dicts_per_thread
+            end_index = (i + 1) * num_dicts_per_thread if i < num_threads - 1 else len(data_list)
+            thread = threading.Thread(target=process_data, args=(data_list, start_index, end_index))
+            threads.append(thread)
+            thread.start()
 
-        with open(f'/data/xuancheng/final_cbtagency/single_turn/{dataset_name}_cbtagency_{language}.json', 'w',
-                  encoding='utf-8') as f:
-            json.dump(result, f, indent=4, ensure_ascii=False)
+        # 等待所有线程完成
+        for thread in threads:
+            thread.join()
